da90faae8cd6ce92749abd768cf9fcd21c51c68b
Tazim <tazimtazim2012@hotmail.com>
2017-09-07 06:12:15 +0600

    Refactored UI folder as APP for MVC2L

---
+++ root/App/SearchResult.php
@@ -0,0 +1,35 @@
+<?php
+	require_once('../index.php');
+	
+	$name="Tushar";
+	$salary="6000Tk";
+	$phoneno="01740072214";
+?>
+
+<body>
+	<form method="post">	
+	<br/>
+	<br/>
+	<table align=center border=1 width=800>
+		
+		<tr>
+			
+			<td>
+				<img src="Resources/pic.jpg" height=80px width=80px>
+			</td>
+		
+			<td>
+				<b>Name: </b> <?php echo $name; ?> <br/>
+				<b>Expected salary: </b><?php echo $salary; ?><br/>
+				<b>Mobile no: </b> <?php echo $phoneno; ?>
+			</td>
+			<td>
+				<input type="button" onClick="location.href='TutorProfile.php'" value='View'>
+			</td>
+			
+		</tr>
+		
+		
+	</table>
+	</form>
+</body>
--- root/index.php
+++ root/index.php
@@ -1,400 +1,184 @@
-<?php
-$name="Samiul Haque Shovon";
-$id=15291721;
-$status="Last year Student @AIUB";
-$bio="Beside doing my study I also do some tutions. I already give tution to many students. I do tution with great 
-						care and can deal with my responsibilty!";
-
-$phone="01758135197";
-$mail="s.h.shovon@gmail.com";
-$address="Rajshahi, Bangladesh";
-$salary=5000;
-$currStatus="Available";
-$day=4;
-$place="Home Visit";
-$medium="Bangla";
-$class="VII,VIII,IX,X,XI,XII";
-$subject="Sciene, Math, English";
-$area="Mirpur,Mohammadpur,Dhanmondi";
-
-$log="00:09:45";
-$activation="04-August-2013";
-$rating=5;
-
-$qualification1="BSc";
-$qualification2="HSC";
-$qualification3="SSC";
-
-$school1="AIUB";
-$school2="Rajshahi Govt. City College";
-$school3="Govt. Laboratory High School";
-
-$year1=2018;
-$year2=2013;
-$year3=2011;
-
-$grade1=3.96;
-$grade2=5.0;
-$grade3=5.0;
-
-$dept1="CSE";
-$dept2="Science";
-$dept3="Science";
-
-
-
-?>
-
 <html>
-<style>
-
-    .container
-    {
-        width:100%;
-        margin-left:12.5%;
-        margin-right:12.5%;
-
-    }
-
-    .wrap
-    {
-        width: 75%;
-    <!--overflow:auto;-->
-        position: absolute;
-        float: center;
-        background:lightgray;
-        height:1000px;
-    }
-
-    .left
-    {
-        float:left;
-        width: 65%;
-
-    }
-
-    .right
-    {
-        float: right;
-        width: 35%;
-    }
-
-    table
-    {
-        border: 1px gray;
-        width: 100%;
-
-    }
-
-    th, td
-    {
-        padding: 7px;
-        text-align: left;
-
-    }
-    tr
-    {
-        background-color: white
-    }
-
-    h1, h2, h3, h4, h5, h6{
-        margin-top:0px;
-        margin-bottom:1px;
-        padding: 0px;
-    }
-
-
-    #one td {
-        border: .5px solid lightgray;
-        padding: 5px;
-    }
-
-    .button
-    {
-        background-color: #4CAF50; /* Green */
-        border: none;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 15px;
-        position: center;
-    }
-
-</style>
-
 
 <body>
+<head>
+    <style>
+        .pp
+        {
+            padding-left: 25px;
+            padding-top: 10px;
+        }
+        .button
+        {
+            padding: 12px 180px;
+            text-align: center;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<form method="post">
+    <div class="pp">  <h1>tutorsHUB</h1> </div>
 
 
-<div class="container">
 
-    <div class="wrap">
-
-        <div class="left">
-            <table style="width=100%" cellspacing="10">
-                <tr>
-                    <td>
-                        <img src="a.jpg"  style="width:200px;height:230px;">
-                    </td>
-
-                    <td>
-                        <h1 style="margin-top:-30px">  <?php echo $name; ?> </h1>
-                        <p style="margin-top: 2px"> <b> ID #<?php echo $id; ?> </b>
-                        </p>
-                        <p> <b>Current Status: </b> <?php echo $status; ?>  </p>
-                        <p> <?php echo $bio; ?> </p>
-                    </td>
-
-                </tr>
-
-                <td  colspan="2">
-                    <p style="margin-top:-20px"> <br> <img src="call.png"  style="width:20px;height:20px;"> +88<?php echo $phone; ?> <img src="email.jpg"  style="width:20px;height:20px;"> <?php echo $mail; ?><img src="add.png"  style="width:20px;height:20px;"> <?php echo $address; ?> </P>
+    <div align="right">
+        <table>
+            <tr>
+                <td>
+                    Email<br/><input type="text" name="email" size="20"  />
                 </td>
-
-            </table>
-
-            <table cellspacing="10">
-
-                <td colspan="2">
-
-                    <table border=10>
-
-                        <h2>
-                            Educational Qualification
-                        </h2>
-                        <br>
-                        <tr>
-                            <th style="text-align:center">Qualification</th>
-                            <th style="text-align:center">School/College/University</th>
-                            <th style="text-align:center">Year of passing</th>
-                            <th style="text-align:center">Grade</th>
-                            <th style="text-align:center">Group/Subject</th>
-
-
-                        </tr>
-
-                        <tr>
-                            <td style="text-align:center"><?php echo $qualification1; ?></td>
-                            <td style="text-align:center"><?php echo $school1; ?></td>
-                            <td style="text-align:center"><?php echo $year1; ?></td>
-                            <td style="text-align:center"><?php echo $grade1; ?></td>
-                            <td style="text-align:center"><?php echo $dept1; ?></td>
-
-
-                        </tr>
-
-                        <tr>
-                            <td style="text-align:center"><?php echo $qualification2; ?></td>
-                            <td style="text-align:center"><?php echo $school2; ?></td>
-                            <td style="text-align:center"><?php echo $year2; ?></td>
-                            <td style="text-align:center"><?php echo $grade2; ?></td>
-                            <td style="text-align:center"><?php echo $dept2; ?></td>
-
-
-                        </tr>
-
-                        <tr>
-                            <td style="text-align:center"><?php echo $qualification3; ?></td>
-                            <td style="text-align:center"><?php echo $school3; ?></td>
-                            <td style="text-align:center"><?php echo $year3; ?></td>
-                            <td style="text-align:center"><?php echo $grade3; ?></td>
-                            <td style="text-align:center"><?php echo $dept3; ?></td>
-
-
-
-                        </tr>
-
-                        </td colspan="2">
-
-                    </table>
-
-            </table>
-
-
-            <table cellspacing="10">
-
 
                 <td>
-                    <h1>
-                        Tution Info
-                    </h1>
-
-
-                    <table id="one">
-                        <tr >
-                            <td style="width:150px">
-                                Expected Salary:
-                            </td>
-
-                            <td>
-                                <?php echo $salary; ?>tk
-                            </td>
-
-                        </tr>
-
-                        <tr>
-                            <td >
-                                Current Status:
-                            </td>
-
-                            <td>
-                                <?php echo $currStatus; ?>
-                            </td>
-
-                        </tr>
-
-                        <tr>
-                            <td>
-                                Days Per Week:
-                            </td>
-
-                            <td>
-                                <?php echo $day; ?> days
-                            </td>
-
-                        </tr>
-
-                        <tr>
-                            <td>
-                                Place of learning:
-                            </td>
-
-                            <td>
-                                <?php echo $place; ?>
-                            </td>
-
-                        </tr>
-
-                        <tr>
-                            <td>
-                                Preferred Medium:
-                            </td>
-
-                            <td>
-                                <?php echo $medium; ?>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td >
-                                Preferred Classes:
-                            </td>
-
-                            <td>
-                                <?php echo $class; ?>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td >
-                                Preferred Subjects:
-                            </td>
-
-                            <td>
-                                <?php echo $subject; ?>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td >
-                                Preferred Area:
-                            </td>
-
-                            <td>
-                                <?php echo $area; ?>
-                            </td>
-                        </tr>
-
-                    </table>
-
+                    Password<br/><input type="password" name="password" size="20" />
                 </td>
 
-            </table>
-
-        </div>
-        <div class="right">
-
-            <table style="margin-top:0px" cellspacing="10">
-
-
-                <td colspan="2">
-
-
-                    <b> Member Since: </b> <?php echo $activation; ?> <br>
-                    <b> Last Login: </b> <?php echo $log; ?> <br>
-
-
-
-                </td>
-            </table>
-
-            <table cellspacing="10">
-
-
-                <td colspan="2">
-
-
-                    <h2> Rating </h2>
-
-
-
-
-                </td>
-
-                <td colspan="2">
-
-                    <h2> <?php echo $rating; ?> </h2>	<br>
-                    <font size="6"> <?php for($i=0;$i<$rating;$i++)echo "* "; ?></font>
-
-
-
+                <td>
+                    <br/>
+                    <button type="submit">Log in</button>
 
                 </td>
 
 
+            </tr>
+            <tr>
+                <td></td>
+                <td align="center">
+                    <a href="App/Registration.php">not registered yet</a>
+                </td>
+            </tr>
 
-            </table>
 
-            <table cellspacing="10">
+        </table>
+
+
+
+
+    </div>
+    <hr>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <div align="center">
+        <button class="button" type="submit" formaction="./App/search.php">Search</button>
+    </div>
+    <br/>
+
+
+    <div align="center">
+        <select>
+            <option value="Mirpur"  style="width: 130">Mirpur</option>
+            <option value="Uttara"  style="width: 130">Uttara</option>
+            <option value="Dhanmondi"  style="width: 130">Dhanmondi</option>
+
+        </select>
+
+        <select>
+            <option value="Male"  style="width: 130">Male</option>
+            <option value="Female"  style="width: 130">Female</option>
+            <option value="Any"  style="width: 130">Any</option>
+
+        </select>
+
+        <select>
+            <option value="one"  style="width: 130">Class 1</option>
+            <option value="two"  style="width: 130">Class 2</option>
+            <option value="three"  style="width: 130">Class 3</option>
+            <option value="four"  style="width: 130">Class 4</option>
+            <option value="five"  style="width: 130">Class 5</option>
+            <option value="six"  style="width: 130">Class 6</option>
+            <option value="seven"  style="width: 130">Class 7</option>
+            <option value="eight"  style="width: 130">Class 8</option>
+            <option value="nine"  style="width: 130">Class 9</option>
+            <option value="ten"  style="width: 130">Class 10</option>
+            <option value="eleven"  style="width: 130">Class 11</option>
+            <option value="twelve"  style="width: 130">Class 12</option>
+
+        </select>
+
+        <label>Salary</label>  min<input type="text" name="min" size="5"  />-
+        <input type="text" name="max" size="5"  />max
+
+
+    </div>
+
+
+
+    <div align="center">
+        <ul>
+            <table>
+                <tr>
+                    <td>Subjects:</td>
+                </tr>
 
 
                 <tr>
-
-                    <td colspan="2">
-                        <h2>
-                            Contact this Tutor
-                        </h2>
-
-                        <table>
-
-
-                            <tr>
-                                <td colspan="2">
-                                    <b> Name <b> <br> <input type="text" name="name" size="35"> <br/>
-                                            <b> Phone No <b> <br> <input type="text" name="phone" size="35"> <br/>
-                                                    <b> Email <b> <br> <input type="text" name="email" size="35"> <br/>
-                                                            <b> Message <b> <br> <input type="text" name="message" size="35" style="height: 60px;"> <br/>								<br>
-                                                                    <button type="button" class="button" >Send Message</button>
-
-                                </td>
-
-
-                            </tr>
-
-                        </table>
+                    <td><input type="checkbox" name="bangla" value="bangla">Bangla</td>
+                    <td><input type="checkbox" name="english" value="english">English</td>
+                    <td><input type="checkbox" name="math" value="math">Math</td>
+                    <td><input type="checkbox" name="ict" value="ict">ICT</td>
 
                 </tr>
-                </td>
+
+
+                <tr>
+                    <td><input type="checkbox" name="religion" value="religion">Religion</td>
+                    <td><input type="checkbox" name="physics" value="physics">Physics</td>
+                    <td><input type="checkbox" name="chemistry" value="chemistry">Chemistry</td>
+                    <td><input type="checkbox" name="biology" value="biology">Biology	</td>
+
+                </tr>
+
+                <tr>
+                    <td><input type="checkbox" name="science" value="science">Social Science</td>
+                    <td><input type="checkbox" name="higherMath" value="higherMath">Higher Math</td>
+                    <td><input type="checkbox" name="career" value="career">Career</td>
+                    <td><input type="checkbox" name="physical" value="physical">Physical Exercise	</td>
+
+                </tr>
 
             </table>
 
-        </div>
+        </ul>
 
     </div>
-</div>
 
+
+
+</form>
 </body>
 
 </html>
+
+<?php
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+    $users = simplexml_load_file("./data.xml");
+
+    $flag=false;
+    $count=0;
+
+    foreach($users->user as $user)
+    {
+        if($user->email==$_POST['email'])
+        {
+            $flag=true;
+            break;
+        }
+        $count = $count+1;
+    }
+
+    if($flag)
+    {
+        if($users->user[$count]->password==$_POST['password'])
+            header("Location:./App/TutorDashBoard.php");
+
+    }
+
+}
+
+?>
