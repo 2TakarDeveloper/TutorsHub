6e58e1f97059d74d150e2274d6b1db7973c0b4cd
shahariartushar <shahariartushar@gmail.com>
2017-09-07 20:14:08 +0600

    Update TutorService.php

--- root/service/TutorService.php
+++ root/service/TutorService.php
@@ -10,21 +10,55 @@
 
 function NewTutor($tutor){
     //this function will take a tutor object and add that to database
+
+    $sql = "INSERT INTO user(Email, Password, MemberSince) VALUES('$tutor->email', '$tutor->password', '$tutor->membersince')";
+    $result = executeSQL($sql);
+
+    $sql = "SELECT * FROM user WHERE Email='$tutor[Email]'";
+    $result = executeSQL($sql);
+
+    $person[id] = mysqli_fetch_assoc($result);
+
+    $sql = "INSERT INTO searchinfo(UserId) VALUES('$tutor[id]')";
+    $result = executeSQL($sql);
+
+    return $result;
 }
 
 
 function UpdateTutor($tutor){
     //Update Key=tutorID;
     //this function will take a tutor object and update that in the database
+
+    $sql = "UPDATE user SET Name='$tutor->name', UserImage='$tutor->image' WHERE Id=$tutor->id";
+    $result = executeSQL($sql);
+
+    $sql = "UPDATE searchinfo SET Availability=0, PreferredLocation='$tutor[location]', PreferredSubjects='$tutor[subjects]', PreferredClasses='$tutor[classes]', PreferredMedium='$tutor[medium]', ExpectedSalary=$tutor[salay] WHERE id=$tutor[id]";
+    $result = executeSQL($sql);
+    return $result;
 }
 
 
-function ValidTutor($userName,$password){
+function ValidTutor($tutor){
     //returns true if username and password is correct
+
+    $sql = "SELECT * FROM user where Email='$tutor->email' AND Password='$tutor->password'";
+    $result = executeSQL($sql);
+    return $result;
 }
 
-function getTutorImage($tutorId){
+function getTutorImage($tutor){
     //return image url
+
+    $sql = "SELECT UserImage FROM user where Id=$tutor->id";
+    $result = executeSQL($sql);
+
+    $tutorImage = mysqli_fetch_assoc($result);
+
+    return $tutorImage;
 }
+
+
+
 
 
