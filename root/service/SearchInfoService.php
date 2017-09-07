64a60bc32a80d6ab81d1fb184abc6f28c18e9e1e
Shahariar <shahariartushar@gmail.com>
2017-09-08 00:09:03 +0600

    updated searchinfo service

--- root/service/SearchInfoService.php
+++ root/service/SearchInfoService.php
@@ -11,6 +11,11 @@
 
 function NewSearchInfo($searchInfo){
     //this function will take a searchInfo object and add that to database
+
+    $sql = "INSERT INTO searchinfo(UserId, Gender, Availability,PrefferedLocation,PrefferedSubjects,PrefferedClasses,PrefferredMedium,ExpextedSalary) VALUES($searchInfo[tutorID], '$searchInfo[gender]', $searchInfo[availability], '$searchInfo[location]', '$searchInfo[subjects]', '$searchInfo[classes]', '$searchInfo[medium]', $searchInfo[salary])";
+    $result = executeSQL($sql);
+
+    return $result;
 }
 
 
@@ -19,9 +24,43 @@ function NewSearchInfo($searchInfo){
 function UpdateSearchInfo($searchInfo){
     //Update Key=tutorID;
     //this function will take a searchInfo object and update that in the database
+
+    $sql = "UPDATE user SET Gender='$searchInfo[gender]', Availability=$searchInfo[availability], PrefferedLocation='$searchInfo[location]', PrefferedSubjects='$searchInfo[subjects]', PrefferedClasses='$searchInfo[classes]', PrefferredMedium='$searchInfo[medium]', ExpextedSalary=$searchInfo[salary]   WHERE UserID=$searchInfo[tutorID]";
+    $result = executeSQL($sql);
+
+
+    return $result;
 }
 
 
 function getSearchInfo($tutorID){
     //GetSearchInfo By tutorID;
+
+    $sql = "SELECT * FROM searchinfo WHERE UserID= $tutorID";
+
+    $result = executeSQL($sql);
+
+    $tutors = array(
+        'UserID'=>'',
+        'Gender'=>'',
+        'Availability'=>'',
+        'PrefferedLocation'=>'',
+        'PrefferedSubjects'=>'',
+        'PrefferedClasses'=>'',
+        'PrefferedMedium'=>'',
+        'ExpectedSalary'=>''
+    );
+
+    for($i=0; $row=mysqli_fetch_assoc($result); ++$i){
+        $tutors['UserId'] = $row['UserId'];
+        $tutors['Gender'] = $row['Gender'];
+        $tutors['Availability'] = $row['Availability'];
+        $tutors['PrefferedLocation'] = $row['PrefferedLocation'];
+        $tutors['PrefferedSubjects'] = $row['PrefferedSubjects'];
+        $tutors['PrefferedClasses'] = $row['PrefferedClasses'];
+        $tutors['PrefferedMedium'] = $row['PrefferedMedium'];
+        $tutors['ExpectedSalary'] = $row['ExpextedSalary'];
+    }
+
+    return $tutors;
 }
