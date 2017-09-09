dfd988ae31663f70b9251631cbe776005ac40294
Shahariar <shahariartushar@gmail.com>
2017-09-09 12:10:52 +0600

    updating LastLogin service

--- root/service/TutorInfoService.php
+++ root/service/TutorInfoService.php
@@ -6,6 +6,11 @@ function updateTutorInfo($userInfo,$userId){
     // and save it with UserID The Path is Predefined but do keep an static string variable
     //This will work both as update or new Entry since it always rewrite everything in the file
 
+    $sql = "UPDATE userinfo SET MobileNo='$userInfo[mobileNo]', Address='$userInfo[address]', CurrentStatus='$userInfo[currentStatus]', Bio='$userInfo[bio]' WHERE TutorId=$userId";
+    $result = executeSQL($sql);
+
+    return $result;
+
 }
 
 function GetTutorInfo($tutorID){
@@ -18,3 +23,15 @@ function GetTutorInfo($tutorID){
     $row=mysqli_fetch_assoc($result);
     return $row;
 }
+
+function UpdateLastLogin($tutorID, $lastLogin){
+    //Return a Tutor info Object searched by the tutorID from local storage
+
+    $sql = "UPDATE userinfo SET LastLogin='$lastLogin' WHERE TutorId=$tutorID";
+
+    $result = executeSQL($sql);
+
+    return $result;
+}
+
+?>
