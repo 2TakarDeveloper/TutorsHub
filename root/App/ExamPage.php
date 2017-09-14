6fe17a1789f8bb167cf4a126e2a43fa81a5e1e10
Tazim <tazimtazim2012@hotmail.com>
2017-09-14 14:46:13 +0600

    Exam page added

---
+++ root/App/ExamPage.php
@@ -0,0 +1,107 @@
+<?php?>
+
+<html>
+
+	<body>
+	<head>
+	<style>
+div.pp
+		{
+            padding-left: 25px;
+			padding-top: 10px;
+		}
+
+
+		.button
+		{
+            background-color:  #277ec8 ;
+    color: white;
+    padding: 16px;
+    font-size: 14px;
+    border: none;
+    cursor: pointer;
+		}
+
+	.button1
+		{
+            background-color:1f9609 ;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+		}
+
+
+
+	</style>
+	</head>
+	<body>
+
+	<div class="pp">  <h1>tutorsHUB</h1>
+
+
+		<div  style="width:70px;">
+			  <button  class="button" style="width:70px;">Back </button>
+
+			</div>
+
+	</div>
+
+
+		  <center><h2>Bangla</h2></center>
+
+
+	<hr>
+
+
+		<br>
+
+		<div align=>
+
+
+			<label>1. What year did Bangladesh finally become independent and gain the name it has today?</label>
+
+			<br>
+
+
+
+			  <input type="radio" name="a" id="a" value="a">//Values must be dynamically set with php
+			   <label for="a">1947</label>
+
+			  <input type="radio" name="b" id="b" value="b">
+			   <label for="b">1971</label>
+
+			  <input type="radio" name="c" id="c" value="c">
+			<label for="c">1952</label>
+
+			<input type="radio" name="d" id="d" value="d">
+			   <label for="d">1954</label>
+
+
+		</div>
+
+
+			<div  align=right>
+			  <button  class="button" style="width:60px;">Next</button>
+
+			</div>
+
+
+			<br>
+			<br>
+			<br>
+			<br>
+
+
+			<div align=center>
+			  <button  class="button1" style="width:100px;">Submit</button>
+
+			</div>
+
+
+
+
+	</body>
+
+</html>
